454ba06859801f71c1af0c8f47d491c788ca74d4
stijn-vh <stijn@zmeel.nl>
2023-10-24 12:17:51 +0200

    add rules

--- query_handler.py
+++ query_handler.py
@@ -3,15 +3,21 @@ from owlready2 import *
 class QueryHandler:
 
     def __init__(self) -> None:
-        self.iris = {}
+        self.iris = {
+            'None': []
+        }
+
+        self.wm = {}
+
         self.onto_iri = 'urn_webprotege_ontology_5d0dac0d-8168-404e-b9c8-c3f420fec954'
         self.onto = get_ontology(self.onto_iri + '.owl').load()
 
         self.fill_iris_dict()
 
+        print()
+
     def fill_iris_dict(self):
         list_of_entities = [self.onto.individuals(), self.onto.classes(), self.onto.object_properties(), self.onto.data_properties()]
-        self.iris['None'] = []
 
         for entities in list_of_entities:
             for entity in entities:
@@ -20,12 +26,30 @@ class QueryHandler:
                 else:
                     self.iris['None'].append(entity.iri)
 
+                if entity in self.onto.data_properties() or entity in self.onto.object_properties():
+                    self.wm[str(entity.label[0])] = [] 
+
     def retrieve_IRI(self, label):
         return self.iris[label]
     
     def print_results(self, results):
         for result in results:
             print(result)
+
+    def get_queries_to_perform(self):
+        queries = []
+
+        if any('Guest' in item for item in self.wm['hasOccupation']):
+            queries.append(self.get_persons_with_place_and_occupation)
+        
+        for item in self.wm['serves']:
+            if any(item[0] is item_2[0] for item_2 in self.wm['dishes']):
+                queries.append(self.get_dishes_from_cuisine_of_restaurant)
+        
+        for item in self.wm['hasHealthCondition']:
+            if any(item[0] is item_2[0] for item_2 in self.wm['consumes']):
+                queries.append(self.get_dishes_from_cuisine_of_restaurant)
+        
     
     ######################## Queries ########################
 
